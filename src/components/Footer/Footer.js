23c38342448271e36742d81d488de79e5ee72956
ventasminetronix <minetronix.control@gmail.com>
2023-07-28 09:43:10 -0500

    proyecto funcionando
    Mernstack con bootstrap

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container, Nav, NavItem, NavLink } from "reactstrap";
+
+function Footer() {
+  return (
+    <footer className="footer">
+            
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/routes.js
@@ -0,0 +1,50 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.js";
+
+import Notifications from "views/Notifications.js";
+
+import UserProfile from "views/UserProfile.js";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: "tim-icons icon-chart-pie-36",
+    component: <Dashboard />,
+    layout: "/admin",
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    rtlName: "إخطارات",
+    icon: "tim-icons icon-bell-55",
+    component: <Notifications />,
+    layout: "/admin",
+  },
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    rtlName: "ملف تعريفي للمستخدم",
+    icon: "tim-icons icon-single-02",
+    component: <UserProfile />,
+    layout: "/admin",
+  },
+];
+export default routes;
